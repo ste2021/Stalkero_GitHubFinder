4867b93bc05b296d390697edf26adddd8caeea02
ste2021 <estela.oliveira.cont@outlook.com>
2020-10-26 22:26:39 -0300

    initial js

---
+++ js/filterscroll.js
@@ -0,0 +1,14 @@
+$("#filtro").keyup(function() {
+            var texto = $(this).val();
+
+            //     $(".perfil-listado").each(function() {
+            //         var resultado = $(this).text().toUpperCase().indexOf(' ' + texto.toUpperCase());
+
+            //         if (resultado < 0) {
+            //             $(this).fadeOut();
+            //         } else {
+            //             $(this).fadeIn();
+            //         }
+            //     });
+
+            // });
